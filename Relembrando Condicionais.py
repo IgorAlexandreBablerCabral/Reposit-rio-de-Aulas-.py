df7a82d77f30b37c60d8fe1f636d4dd2e3510ee7
Igor Alexandre Babler Cabral <81613814+IgorAlexandreBablerCabral@users.noreply.github.com>
2025-09-11 00:01:50 -0300

    Create Relembrando Condicionais.py

---
+++ Relembrando Condicionais.py
@@ -0,0 +1,32 @@
+// Condicional
+# if
+if 5 > 10 :
+  print('Verdade')
+
+// If Else
+num1 = 10
+num2 = 5
+
+if num1> num2:
+  print('O num1 é maior número')
+else:
+  print('O num2 é o maior número')
+
+// If// Elif / Else
+num1 = 10
+num2 = 10
+if num1 > num2:
+  print('O num1 é o maior número')
+elif num1 < num2:
+  print('O num2 é o maior número')
+else:
+  print('Os dois números são iguais')
+
+//Condicional com operadores Lógicos
+num1 = 10
+num2 = 5
+num3 = 2
+if (num1 > num2) and (num2 > num3):
+  print(' O num1 é o maior número')
+else:
+  print('O num1 não é o maior número')
